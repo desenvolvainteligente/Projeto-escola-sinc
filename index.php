1b1e9ba0e76fb67457e476ffaaf103847063a951
desenvolvainteligente <desenvolvainteligente@gmail.com>
2021-05-27 08:59:56 -0300

    Início do projeto escola sinc

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Escola Sincronizada</title>
+
+        <meta charset="utf-8">
+        <meta content="author" name="Thiago S. Viana">
+        <meta content="keyword" name="Escola, Escola Sincronizada">
+        <meta content="viewport" name="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" href="css/style.css">
+
+        <script src="js/script.js"></script>
+    </head>
+    <body>
+        <header>
+            <!-- Aqui vai ficar o cabeçalho do nosso sistema e tudo pertinente ao titulo do projeto -->
+        </header>
+        <nav>
+            <!-- Aqui vai ficar o menu de navegação do sistema -->
+            <ul>
+
+            </ul>
+        </nav>
+        <section>
+            <?php var_dump($_GET); ?>
+            <!-- Aqui vai ficar todo o conteúdo do sistema -->
+        </section>
+        <aside>
+            <!-- Aqui vai ficar a barra lateral, com algumas informações pertinentes as áreas do sistema -->
+        </aside>
+        <footer>
+            <!-- Aqui vai ser o rodapé do sistema -->
+        </footer>
+    </body>
+</html>
